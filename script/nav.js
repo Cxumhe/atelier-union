8e3b751600278ea8bc59dbeeb6184998ae2be9e5
Cxumhe <2213145737@qq.com>
2023-12-14 16:04:49 +0800

    fix nav transition bug

--- script/nav.js
+++ script/nav.js
@@ -11,12 +11,14 @@ const html = document.querySelector("html");
 const logo = document.querySelector("nav header a img");
 
 navToggle.addEventListener("change", () => {
+    navul.style.transition = ".2s";
+
     if(navToggle.checked) {
         navul.style.height = "auto";
         // 解构赋值
         const { height } = navul.getBoundingClientRect();
         navul.style.height = 0;
-        navul.style.transition = ".2s";
+        // 强行渲染一次，否则直接渲染最终状态，随便读一个数值
         navul.offsetHeight;
         navul.style.height = height + "px";
 
@@ -28,6 +30,7 @@ navToggle.addEventListener("change", () => {
         spans[1].style.transform = 'rotate(135deg)';
         spans[2].style.transform = 'rotate(45deg)';
     } else {
+        navul.style.transition = ".2s";
         navul.style.height = 0;
         for(let span of spans) {
             span.style.transform = 'none';
@@ -36,7 +39,14 @@ navToggle.addEventListener("change", () => {
         spans[1].style.top = 18.26 + 'px';
         spans[2].style.top = 28.26 + 'px';
     }
+    navul.addEventListener("transitionend", transitionEndHandler, { once: true });
 })
+
+    // 过渡结束后删除transition
+function transitionEndHandler() {
+    navul.style.transition = "";
+}
+
 
 window.onresize = function() {
     // console.log(document.body.clientWidth);
