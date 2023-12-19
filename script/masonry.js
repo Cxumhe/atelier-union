8060b8219ae9e064204dc80cf94a5f2145476240
Cxumhe <2213145737@qq.com>
2023-12-19 20:14:11 +0800

    1

--- script/masonry.js
+++ script/masonry.js
@@ -6,8 +6,8 @@ const imgWidth = 250;
 //针对不同网页获取照片
 const url = new URL(location.href);
 const index = url.pathname.lastIndexOf("/");
-// const indexEnd = url.pathname.indexOf(".");
-const indexEnd = url.pathname.length;
+const indexEnd = url.pathname.indexOf(".");
+// const indexEnd = url.pathname.length;
 const imgDate = url.pathname.slice(index + 1, indexEnd).replace("_", ".");
 
 fetch(`../image/gallery/${imgDate}/info.json`)
@@ -96,6 +96,17 @@ function preview() {
         // 点击后触屏平台终止默认行为
         container.ontouchmove = function (e) {
             e.preventDefault();
+            // 倒数两个数
+            const width = getComputedStyle(innerImg).width.slice(0, -2);
+            const height = getComputedStyle(innerImg).height.slice(0, -2);
+            
+            if(e.deltaY > 0) {
+                innerImg.style.width = parseInt(width) * 1.2 + "px";
+                innerImg.style.height = parseInt(height) * 1.2 + "px";
+            } else if (e.deltaY < 0 && width > 200) {
+                innerImg.style.width = parseInt(width) * 0.8 + "px";
+                innerImg.style.height = parseInt(height) * 0.8 + "px";
+            }
         }
         container.onclick = function() {
             container.remove();
